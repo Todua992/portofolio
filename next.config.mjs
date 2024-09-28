9d534230eef5566740ec8dcff6b0063898aad05a
Todua992 <todualuka992@gmail.com>
2024-09-28 02:36:54 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,
   images: {
     domains: ['aceternity.com', 'images.unsplash.com', 'assets.aceternity.com'],
   },};
